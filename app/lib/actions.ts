582bf9865259e6506b53255bb41578109cf625d9
Vintanina <vitorhvip@gmail.com>
2025-03-17 14:31:12 +0300

    delete and edit invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,7 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Create Invoices
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -34,3 +35,31 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+// Edit Invoices
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+// Delete Invoice
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+
